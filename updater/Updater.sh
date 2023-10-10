d8738d9bbbd68340a8ee22ea9ce7281f2c1dab7e
Kevin Huddleston <kevhuddleston@gmail.com>
2023-10-10 14:04:30 -0500

    fixed formatting errors.

--- updater/Updater.sh
+++ updater/Updater.sh
@@ -10,33 +10,34 @@ version=/etc/os-release
 
 echo "Hello, welcome to my updater script."
 echo 
+sleep 1
 
 echo "The current system time and date is: $now"
 echo "You are currently logged into: $HOSTNAME"
 echo "You are currently logged in as: $USER"
 
 #This section is only run if the system is using Pacman.
-if grep -q "Arch" $version_file
+if grep -q "Arch" $version
 then
 	sudo pacman -Syu
 fi
 
 #This section is only run if the system is using the APT manager.
-if grep -q "Debian" $version_file || grep -q "Ubuntu" $version_file
+if grep -q "Debian" $version || grep -q "Ubuntu" $version
 then
 	sudo apt update
 	sudo apt upgrade
 fi
 
 #This section is only run if the system uses the Portage package manager.
-if grep -q "Gentoo" $version_file
+if grep -q "Gentoo" $version
 then
 	sudo emerge --sync
 	sudo emerge --ask --update --deep --newuse @world
 fi
 
 #This section is only run if the system uses the DNF package manager.
-if grep -q "Rocky" $version_file || grep -q "RHEL" $version_file
+if grep -q "Rocky" $version || grep -q "RHEL" $version
 then
 	sudo dnf update
 fi
